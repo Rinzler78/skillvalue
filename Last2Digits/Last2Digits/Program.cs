78b59db60f2299ae77b34fd52a83cf84c20ebbbf
Boris LECLERE <Borisleclere@gmail.com>
2018-10-18 18:20:28 +0200

    Add Last2Digits Add SmallerPrimes

---
+++ Last2Digits/Last2Digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Last2Digits
+{
+    public class Last2DigitsClass
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader sr;
+            int a, b;
+            int result = -1;
+            try
+            {
+                a = 10;
+                b = 11;
+
+                var resStr = (a * b).ToString();
+
+                int.TryParse(resStr.Substring(resStr.Length - 2, 2), out result);
+
+                Console.WriteLine("{0}", result);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SmallerPrimes/SmallerPrimes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace SmallerPrimes
+{
+    public class SmallerPrimes
+    {
+        static void Main(string[] args)
+        {
+            int N = 23;
+            int[] result = new int[0];
+            //var lines = File.ReadAllLines(args[0]);
+            //int.TryParse(lines[0], out N);
+
+            var intList = new List<int>();
+
+            for (int i = 2; i < N; ++i)
+            {
+                if(IsPrimeNumber(i))
+                    intList.Add(i);
+            }
+
+            result = intList.ToArray();
+
+            for (int j = 0; j < result.Length; j++)
+            {
+                Console.Write(result[j]);
+                if (j < result.Length - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static bool IsPrimeNumber(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i < number; ++i)
+            {
+                if ((number % i) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
